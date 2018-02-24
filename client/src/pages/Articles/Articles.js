2f01b3ac93e20a101788f9969028489894074b9a
Brandon York <30988963+byork2005@users.noreply.github.com>
2018-02-23 21:31:34 -0600

    mvc setup, backend done, needs frontend

---
+++ client/src/pages/Articles/Articles.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import DeleteBtn from "../../components/DeleteBtn";
+import API from "../../utils/API";
+import { Link } from "react-router-dom";
+import { Col, Row, Container } from "../../components/Grid";
+import { List, ListItem } from "../../components/List";
+import { Input, TextArea, FormBtn } from "../../components/Form";
+import default from "../../utils/API";
+import logo from '../components/logo.svg';
+import '../../App.css';
+
+class Articles extends Component {
+
+
+  render() {
+    return (
+      <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Welcome to React</h1>
+      </header>
+      <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+    </div>
+    )
+  }
+
+}
+
+export default Articles;
